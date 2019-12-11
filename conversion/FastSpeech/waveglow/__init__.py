59e861103c1d51847f83c44ddd8e9795a5e3927d
zhuribing <zhuribing@vipkid.com.cn>
2019-12-11 16:37:49 +0800

    add

--- conversion/FastSpeech/waveglow/__init__.py
+++ conversion/FastSpeech/waveglow/__init__.py
@@ -1,6 +1,9 @@
 # import waveglow.inference
 # import waveglow.mel2samp
 # import waveglow.glow
-from . import inference as waveglow_inference
-from . import mel2samp as waveglow_mel2samp
-from . import glow as waveglow_glow
+# from . import inference as waveglow_inference
+# from . import mel2samp as waveglow_mel2samp
+# from . import glow as waveglow_glow
+from . import inference
+from . import mel2samp
+from . import glow 
